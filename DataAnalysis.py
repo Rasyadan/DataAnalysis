0729bf3a63c22cf366beec722b4dc9fc8d865393
Rasyadan <88817085+Rasyadan@users.noreply.github.com>
2021-11-28 13:17:17 +0700

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -117,23 +117,40 @@ print("=" * 50)
 
 #Visualisasi Data
 print("TUGAS 6 : Visualisasi Data")
-# Histogram dari Review Score untuk mengetahui persebaran data
-df_review_sorted = Data.sort_values(["Metrics.Review Score"], ascending=[1])
-df_review_sorted["Metrics.Review Score"].plot(kind="hist",bins=[0,10,20,30,40,50,60,70,80,90,100], rwidth = 0.8, title = "Histogram of Review Score", xlabel = "Review Score", legend = True)
+# Histogram dari Review Score dan Sales untuk mengetahui persebaran data
+df_review_sorted1 = Data.sort_values(["Metrics.Review Score"], ascending=[1])
+df_review_sorted1["Metrics.Review Score"].plot(kind="hist",bins=[0,10,20,30,40,50,60,70,80,90,100], rwidth = 0.8, title = "Histogram of Review Score", xlabel = "Review Score", legend = True)
 plt.legend(["Review Score"])
 plt.show()
 
-# Line graph untuk mengetahui perkembangan review score dari tahun ke tahun
+Data["Metrics.Sales"] = Data["Metrics.Sales"]/1000
+df_review_sorted2 = Data.sort_values(["Metrics.Sales"], ascending=[1])
+df_review_sorted2["Metrics.Sales"].plot(kind="hist",bins=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], rwidth = 0.8, title = "Histogram Total Penjualan", xlabel = "Total Penjualan", legend = True)
+plt.legend(["Total Penjualan (Juta USD)"])
+plt.show()
+
+# Line graph untuk mengetahui perkembangan review score dan sales dari tahun ke tahun
 year_mean_review_score = (Data.groupby("Release.Year")["Metrics.Review Score"].mean())
 df2 = year_mean_review_score
 df2.plot(kind="line", x="Release.Year", legend=True, xlabel = "Release Year", ylabel = "Review Score", title ="Review Score Setiap Tahun")
 plt.legend(["Review Score"])
 plt.show()
 
-# Pie chart dari rating game untuk mengetahui hierarki dan hubungan keseluruhan bagian
+year_mean_sales = (Data.groupby("Release.Year")["Metrics.Sales"].mean())
+dfsales = year_mean_sales
+dfsales.plot(kind="line", x="Release.Year", legend=True, xlabel = "Release Year", ylabel = "Total Penjualan", title ="Perkembangan Sales Setiap Tahun")
+plt.legend(["Total Penjualan"])
+plt.show()
+
+# Pie chart dari rating game dan tahun rilis game untuk mengetahui hierarki dan hubungan keseluruhan bagian
 df3 = Data["Release.Rating"].value_counts()
-df3.plot(kind="pie", title="Pie Chart of Game Ratings", y="Release Rating", legend=True)
+df3.plot(kind="pie", title="Pie Chart Rating Game", y="Release Rating", legend=True)
 plt.legend(["Everyone", "Teen", "Mature"])
+plt.show()
+
+dfyear = Data["Release.Year"].value_counts()
+dfyear.plot(kind="pie", title="Pie Chart Tahun Perilisan Game", y="Tahun Rilis", legend=True)
+plt.legend(["2007","2008","2006","2005","2004"])
 plt.show()
 
 # Scatter plot dari review score dan total penjualan untuk mengetahui hubungan antar keduanya
@@ -141,12 +158,27 @@ df4 = Data
 df4.plot(kind="scatter", x="Metrics.Review Score",xlabel = "Review Score", y="Metrics.Sales", ylabel = "Total Penjualan (Miliar USD)", title ="Scatter Plot Review Score dan Total Penjualan")
 plt.show()
 
+#Scatter plot dari total penjualan dan used price
+dfscatter2 = Data
+dfscatter2["Metrics.Used Price"] = dfscatter2["Metrics.Used Price"]/100
+dfscatter2.plot(kind="scatter", x="Metrics.Used Price", xlabel="Total Penjualan (Miliar USD)", y="Metrics.Sales", ylabel="Harga Bekas", title="Scatter Plot Total Penjualan dengan Harga Bekas")
+plt.show()
+
 #Korelasi review score dengan total penjualan
 print("TUGAS 7 : Korelasi")
 correlation = df4["Metrics.Review Score"].corr(df4["Metrics.Sales"])
-print("Koefisien korelasinya adalah " + str(correlation))
+print("Koefisien korelasi antara review score dengan penjualan adalah " + str(correlation))
 print("="*50)
 #Visualisasi korelasi dengan scatter plot
 df4 = Data
 df4.plot(kind="scatter", x="Metrics.Review Score",xlabel = "Review Score", y="Metrics.Sales", ylabel = "Total Penjualan (Miliar USD)", title ="Scatter Plot Review Score dan Total Penjualan")
+plt.show()
+#Korelasi total penjualan dengan harga bekas
+corr2 = dfscatter2["Metrics.Used Price"].corr(dfscatter2["Metrics.Sales"])
+print("Koefisien korelasi antara penjualan dengan harga bekas adalah " + str(corr2))
+print("="*50)
+#Visualisasi korelasi
+dfscatter2 = Data
+dfscatter2["Metrics.Used Price"] = dfscatter2["Metrics.Used Price"]/100
+dfscatter2.plot(kind="scatter", x="Metrics.Used Price", xlabel="Total Penjualan (Miliar USD)", y="Metrics.Sales", ylabel="Harga Bekas", title="Scatter Plot Total Penjualan dengan Harga Bekas")
 plt.show()
